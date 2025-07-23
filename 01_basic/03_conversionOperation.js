41fc0144c0e8acda2e247ee2cca6936812943aef
Anshulkotiya <anshulkotiya849@gmail.com>
2025-07-23 22:06:29 +0000

    comparision

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,61 +1,107 @@
-let score="33";
+// let score="33";
 
 
-console.log(typeof score);
+// console.log(typeof score);
 
-console.log(typeof(score));
+// console.log(typeof(score));
 
-let valueInNumber=Number (score)  // change string into number type conversion
+// let valueInNumber=Number (score)  // change string into number type conversion
 
-// example 
+// // example 
 
-a=3;
-b = valueInNumber+a;
+// a=3;
+// b = valueInNumber+a;
 
-c=score+a;
+// c=score+a;
 
-console.log(typeof(valueInNumber));
-console.log(valueInNumber);
-
-
-console.log(b);
-console.log(c);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
 
 
-// score = "33" => 33  ,type-Number
-
-// score = "33ab" => NaN , type-Number 
-
-
-// score=true => 1 ,type - Number
-
-// score= null => 0 ,type -Number
-
-// score= undefined => NaN , type -Number
-
-let isLoggedIn=1;
-
-let booleanIsLoggedIn=Boolean(isLoggedIn);
-
-console.log(typeof(booleanIsLoggedIn)) // type - boolean 
-
-console.log(booleanIsLoggedIn) //  O/P - true
+// console.log(b);
+// console.log(c);
 
 
-// empty string => false
+// // score = "33" => 33  ,type-Number
 
-//1=> true ; 0 => false
+// // score = "33ab" => NaN , type-Number 
 
-// isLoggedIn= "anshul" => true
 
-let someNumber=33
+// // score=true => 1 ,type - Number
 
-let change=String(someNumber);
+// // score= null => 0 ,type -Number
 
-console.log(change);
+// // score= undefined => NaN , type -Number
 
-console.log(typeof(change)); // type- String
-console.log(typeof(someNumber)); // type-Number
+// let isLoggedIn=1;
+
+// let booleanIsLoggedIn=Boolean(isLoggedIn);
+
+// console.log(typeof(booleanIsLoggedIn)) // type - boolean 
+
+// console.log(booleanIsLoggedIn) //  O/P - true
+
+
+// // empty string => false  
+
+// //1=> true ; 0 => false
+
+// // isLoggedIn= "anshul" => true
+
+// let someNumber=33
+
+// let change=String(someNumber);
+
+// console.log(change);
+
+// console.log(typeof(change)); // type- String
+// console.log(typeof(someNumber)); // type-Number
+
+
+// ************** Operations *****************
+
+// let value = 3
+// let negValue=-value;
+
+// console.log(negValue);
+
+
+
+// let str1="hello";
+// let str2=" anshul"
+
+// let str3=str1+str2;
+
+// console.log(str3); // hello anshul 
+
+// console.log("1"+2); // => 12
+
+// console.log("1"+2+2); // => 122
+
+// console.log(1+2+"2"); // O/P- 32
+
+
+// console.log(+true); // => 1
+
+// console.log(+""); // => 0
+
+let num1, num2,num3;
+
+num1=num2=num3=2+2
+
+
+let gameCounter=100;
+
+gameCounter++;  // adding 1 in gamecounter   it return a value before increment
+
+
+
+++gameCounter;  // adding 1 in gamecounter  it return a value after increment
+
+console.log(gameCounter); // =>101
+
+
+
 
 
 
