c23cf9c7a7eb4f50c6575c301006afbf54b1ec62
Anshulkotiya <anshulkotiya849@gmail.com>
2025-07-28 10:49:21 +0000

    0bject half

---
+++ 02_basic/03_object.js
@@ -0,0 +1,29 @@
+// singleton (made by constructor)
+//Object.create   // it is constructor
+
+const JdUser= {
+
+    name: "anshul",
+    
+    "full name":"Anshul Kotiya",
+
+    age:22,
+
+    location:"uttrakhand",
+
+    email:"anshulkotiya213@google,com",
+
+    isLoggedIn:false,
+    LastloginDays:["monday","Saturday"]
+
+}
+
+// console.log(JdUser.name); // way to access the object
+
+console.log(JdUser["full name"]); // way to access the object because the it is note
+
+// console.log(JdUser["email"]); // way to access the object
+
+
+myArray=["h","i"]
+
